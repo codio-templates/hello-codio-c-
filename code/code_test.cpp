3de8dd58243273cc365a5c695b88a4861b002313
Patrick Ester <patricke@codio.com>
2020-03-16 20:48:56 +0000

    fix typo

--- code/code_test.cpp
+++ code/code_test.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(int argc, char **argv) 
 { 
-	int num = stoi(argv[1]); // typecast the string to an int
+  int num = stoi(argv[1]); // typecast the string to an int
   cout << num * 3;         // print the int multiiplied by 3
 
-	return 0; 
+  return 0; 
 } 
