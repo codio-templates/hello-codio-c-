34a4d164d23d390bc855ecf18368e9440f386fa6
Sharon Jason <sjason@codio.com>
2020-03-20 20:28:22 +0000

    got rid of code and answers

--- code/code_test.cpp
+++ code/code_test.cpp
@@ -1,10 +1,1 @@
-#include <iostream> 
-using namespace std; 
-
-int main(int argc, char **argv) 
-{ 
-	int num = stoi(argv[1]); // typecast the string to an int
-  cout << num * 3;         // print the int multiiplied by 3
-
-	return 0; 
-} 
+git a
